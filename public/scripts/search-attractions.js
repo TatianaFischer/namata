25920be417c4d9d3ee9709d11b182f6dfa52278f
Pedro de Paula Maia <suportedsat5@dimep.com.br>
2020-10-14 21:24:04 -0300

    Implementacao completa do maps

--- public/scripts/search-attractions.js
+++ public/scripts/search-attractions.js
@@ -34,7 +34,7 @@ function onButtonClick() {
     getUserLocation(map);
 
     L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGVkcm9wbWFpYSIsImEiOiJja2dhMWEwMTAwMnEzMnNvam1yZGdsdWxkIn0.8gJxjI3utHhuGUcc-yIVKA', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
